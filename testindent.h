319f435a3c73dc12badc1ed14b5271941c3ccbe4
marsp0 <martin.n.spasov@gmail.com>
2021-07-03 10:13:15 +0300

    test header file indentation

---
+++ testindent.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <stdint.h>
+
+#include "Vector.hpp"
+
+class Model {
+	public:
+
+		// Methods
+		Model();
+		~Model();
+		void update(float deltaTime);
+
+		// Data
+		std::vector<Vector4f> 	vertices;
+		std::vector<uint8_t>	colors;
+
+	private:
+};
